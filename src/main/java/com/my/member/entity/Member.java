e5adfd5e7cb5631c8ecbd88472728191b3a60590
choyoonseo12 <choyoonseo1@icloud.com>
2025-08-13 15:56:51 +0900

    초기작업완료

---
+++ src/main/java/com/my/member/entity/Member.java
@@ -0,0 +1,17 @@
+package com.my.member.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column()
+    private Long id;
+    @Column(length = 50, nullable = false)
+    private String name;
+    private int age;
+    private String address;
+}
