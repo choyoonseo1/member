d221b177c82a9c7bef46b60f451c17094b16d3e2
choyoonseo12 <choyoonseo1@icloud.com>
2025-08-13 17:01:44 +0900

    1. service개발 2. controller 개발 -컨트롤러에서 서비스에 전체데이터 가져오기 요청 3.showMember.html에 뿌려주기 완료

--- src/main/java/com/my/member/controller/MemberController.java
+++ src/main/java/com/my/member/controller/MemberController.java
@@ -1,12 +1,28 @@
-package com.my.member.controller
+package com.my.member.controller;
 
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
+import com.my.member.entity.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import service.MemberService;
+
+import java.util.List;
 
 @Controller
-class MemberController {
+public class MemberController {
+    // 서비스를 가져와 실행 할 준비를 한다.
+    // 1. @Autowired 사용
+    @Autowired
+    MemberService service;
+
     @GetMapping("/list")
-    public String showList(){
+    public String showList(Model model){
+        model.addAttribute("title", "리스트보기");
+        // 서비스에 멤버리스트 정보 요청
+        List<Member> memberList = service.getAllList();
+        model.addAttribute("list", memberList);
         return "showMember";
     }
 }
