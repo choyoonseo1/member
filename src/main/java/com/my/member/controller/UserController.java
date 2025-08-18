b7d5ae67d74048d16596e9d6df095c48a760bd94
choyoonseo12 <choyoonseo1@icloud.com>
2025-08-18 17:51:44 +0900

    로그인 세션처리 수정 완료

---
+++ src/main/java/com/my/member/controller/UserController.java
@@ -0,0 +1,95 @@
+package com.my.member.controller;
+
+import com.my.member.dto.UserDto;
+import com.my.member.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    // UserService를 생성자주입 방식으로 추가
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("login")
+    public String loginForm() {
+        return "/user/login";
+    }
+
+    @GetMapping("signup")
+    public String signupForm(Model model) {
+        model.addAttribute("user", new UserDto());
+        return "/user/signup";
+    }
+
+    @PostMapping("signup")
+    public String signup(@ModelAttribute("user") UserDto dto) {
+        userService.saveUser(dto);
+        return "redirect:/";
+    }
+
+
+    @GetMapping("list")
+    public String userList(Model model) {
+        List<UserDto> list = userService.findAllUser();
+        model.addAttribute("list", list);
+        return "/user/userList";
+    }
+
+    @PostMapping("delete")
+    public String userDelete(@RequestParam("email") String email) {
+        userService.deleteUser(email);
+        return "redirect:/user/list";
+    }
+
+    @PostMapping("update")
+    public String updateUserForm(@RequestParam("email") String email
+            , Model model) {
+        //자료를 하나 찾는다.
+        UserDto user = userService.findOneUser(email);
+        model.addAttribute("user", user);
+        return "/user/userUpdate";
+    }
+
+    @PostMapping("updateUser")
+    public String updateUser(@ModelAttribute("user") UserDto user) {
+        userService.saveUser(user);
+        return "redirect:/user/list";
+    }
+
+    @PostMapping("login")
+    public String login(UserDto dto, HttpSession session) {
+        // dto.email을 갖고 User 검색
+        UserDto loginResult = userService.findOneUser(dto.getEmail());
+        // 해당 유저가 있는지 확인한다.
+        if (ObjectUtils.isEmpty(loginResult)) {
+            return "/user/login";
+        } else if (dto.getPassword().equals(loginResult.getPassword())) {
+            // pw가 맞는지 확인한다.
+            // 맞으면 세션을 만들어준다.
+            session.setAttribute("loginEmail", dto.getEmail());
+            // 세션 유지 시간
+            session.setMaxInactiveInterval(60*30);
+            return "redirect:/";
+        } else{
+            // 틀리면 login form 확인
+            return "/user/login";
+        }
+    }
+
+    @GetMapping("logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+        //세션삭제메서드
+        return "main";
+    }
+}
